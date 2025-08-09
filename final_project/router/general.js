7b2c0b5773036a6bb6c6d1fdd7a152e0327c5a75
abhisheksatheesh <abhisheksatheesh92@gmail.com>
2025-08-09 08:19:53 -0400

    Task 2: Implemented GET /isbn/:isbn route to return book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,7 +19,14 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn; // retrieve ISBN from request URL
+  const book = books[isbn];     // find book in the books object
+
+  if (book) {
+    return res.status(200).send(JSON.stringify(book, null, 4));
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
  });
   
 // Get book details based on author
